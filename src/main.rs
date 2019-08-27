b2e48a3c22b29c227aaa871b01ddd3199434abf6
Jason Olson <jolson88@outlook.com>
2019-08-26 22:41:01 -0700

    Reads beam header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use byteorder::{BigEndian, ReadBytesExt};
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::str;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let file = File::open(filename)?;
+
+    println!("Analyzing file '{}'", filename);
+    read(BufReader::new(file))?;
+    Ok(())
+}
+
+fn read(mut reader: BufReader<File>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut iff_header = [0u8; 4];
+    reader.read_exact(&mut iff_header)?;
+    let size = reader.read_u32::<BigEndian>()?;
+    let mut form_type = [0u8; 4];
+    reader.read_exact(&mut form_type)?;
+
+    println!(
+        "Iff: {:?}\nForm: {:?}\nSize: {} bytes\n",
+        str::from_utf8(&iff_header)?,
+        str::from_utf8(&form_type)?,
+        size
+    );
+    Ok(())
 }
