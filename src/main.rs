fe540182c7361519d56c0dcc182f2f65a1bcb1f6
Jason Olson <jolson88@outlook.com>
2019-08-30 21:14:00 -0700

    Read all chunks from file

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use byteorder::{BigEndian, ReadBytesExt};
 use std::convert::TryInto;
 use std::env;
 use std::fs::File;
-use std::io::{BufReader, Read};
-use std::io::{Seek, SeekFrom};
+use std::io::{BufReader, Read, Seek, SeekFrom};
 use std::str;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -38,10 +37,11 @@ fn read_header(reader: &mut BufReader<File>) -> Result<u32, Box<dyn std::error::
     reader.read_exact(&mut form_type)?;
     assert_eq!(b"BEAM", &form_type);
 
-    Ok(size)
+    // Exclude the "BEAM" constant and return the number of bytes _left_ to read
+    Ok(size - 4)
 }
 
-fn read_chunk(reader: &mut BufReader<File>) -> Result<Chunk, Box<dyn std::error::Error>> {
+fn read_chunk(reader: &mut BufReader<File>) -> Result<(Chunk, u32), Box<dyn std::error::Error>> {
     let mut name = [0u8; 4];
     reader.read_exact(&mut name)?;
     let size = reader.read_u32::<BigEndian>()?;
@@ -54,41 +54,25 @@ fn read_chunk(reader: &mut BufReader<File>) -> Result<Chunk, Box<dyn std::error:
     let padding = (4 - (size % 4)) % 4;
     reader.seek(SeekFrom::Current(padding.try_into()?))?;
 
-    Ok(Chunk { name, size, data })
+    // Total bytes read is name field (4 bytes) + size field (4 bytes) + size of chunk + padding
+    Ok((Chunk { name, size, data }, 4 + 4 + size + padding))
 }
 
 impl BeamData {
     pub fn from_file(reader: &mut BufReader<File>) -> Result<BeamData, Box<dyn std::error::Error>> {
-        let size = read_header(reader)?;
+        let size_to_read = read_header(reader)?;
 
-        let chunk = read_chunk(reader)?;
-        println!(
-            "Found '{}' chunk | {} bytes",
-            str::from_utf8(&chunk.name)?,
-            chunk.size
-        );
+        let mut total_read = 0;
+        while total_read < size_to_read {
+            let (chunk, bytes_read) = read_chunk(reader)?;
+            total_read += bytes_read;
+            println!(
+                "Found '{}' chunk | {} bytes",
+                str::from_utf8(&chunk.name)?,
+                chunk.size
+            );
+        }
 
-        let chunk = read_chunk(reader)?;
-        println!(
-            "Found '{}' chunk | {} bytes",
-            str::from_utf8(&chunk.name)?,
-            chunk.size
-        );
-
-        let chunk = read_chunk(reader)?;
-        println!(
-            "Found '{}' chunk | {} bytes",
-            str::from_utf8(&chunk.name)?,
-            chunk.size
-        );
-
-        let chunk = read_chunk(reader)?;
-        println!(
-            "Found '{}' chunk | {} bytes",
-            str::from_utf8(&chunk.name)?,
-            chunk.size
-        );
-
-        Ok(BeamData { size })
+        Ok(BeamData { size: size_to_read })
     }
 }
